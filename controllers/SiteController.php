7e5c688d24f2b260e8c643aaace77d34d632ba9b
codekot <boobookish@gmail.com>
2021-10-11 17:45:56 +0400

    Adding model parameters on home page

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\classes\Config;
 use app\classes\Game;
 use app\web\Controller;
 
@@ -9,7 +10,15 @@ class SiteController extends Controller
 {
     public static function home(){
         $content = Game::process();
+        $c = Config::getInstance();
+        $data = get_class_vars(get_class($c));
+        if(!$data){
+            $data = "something goes wrong";
+        } else {
+            $data = json_encode($data);
+        }
         $params = [
+            'data' => $data,
             'content' => $content
         ];
         return static::render('home', $params);
--- views/home.php
+++ views/home.php
@@ -6,6 +6,7 @@
 <h2>OneMax Genetic Algorithm Implementation</h2>
 
 <?php
+echo $data;
 echo $content;
 ?>
 
